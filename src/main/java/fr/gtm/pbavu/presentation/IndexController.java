a0dee14d7d073460f3d5bc9b6b8f26af7b600309
ArnaudVallette <vallette.arnaud@gmail.com>
2018-07-26 16:05:29 +0200

    index controller WIP

--- src/main/java/fr/gtm/pbavu/presentation/IndexController.java
+++ src/main/java/fr/gtm/pbavu/presentation/IndexController.java
@@ -1,31 +1,43 @@
 package fr.gtm.pbavu.presentation;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import fr.gtm.pbavu.domain.Sondage;
+import fr.gtm.pbavu.service.SondageService;
 
 /**
- * 
+ *
  * @author AZERI-VALLETTE-USSUNET
  *
  */
+
 @Controller
 public class IndexController {
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(IndexController.class);
-	
-	
+
+	@Autowired
+	private SondageService sondService;
+
 	/**
+	 * permet l'ffaichage de la JSP index (dashboard gestion sondage) sur l'url / ou
+	 * /index
 	 * 
 	 * @return la page Index.html
 	 */
-	@RequestMapping("/")
-	public ModelAndView index() {
-		final ModelAndView mav = new ModelAndView("index");
-		return mav;
+	@RequestMapping({ "/index", "/" })
+	public String index(@RequestParam final Model model) {
+		final List<Sondage> sondages = this.sondService.getList();
+		model.addAttribute("sondages", sondages);
+		return "index";
 	}
-	
-	
+
 }
--- src/main/java/fr/gtm/pbavu/service/SondageService.java
+++ src/main/java/fr/gtm/pbavu/service/SondageService.java
@@ -1,7 +1,10 @@
 package fr.gtm.pbavu.service;
 
+import org.springframework.stereotype.Service;
+
 import fr.gtm.pbavu.domain.Sondage;
 
+@Service
 public class SondageService extends CRUDService<Sondage> {
 
 }
