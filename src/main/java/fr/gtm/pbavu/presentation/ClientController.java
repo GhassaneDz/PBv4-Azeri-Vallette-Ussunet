510472cbf94d0e669357aa923aeb2c2b7d7ad687
badrjee <badro_87@yahoo.com>
2018-07-26 17:15:32 +0200

    ajout de Client webSerive

---
+++ src/main/java/fr/gtm/pbavu/presentation/ClientController.java
@@ -0,0 +1,56 @@
+package fr.gtm.pbavu.presentation;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import fr.gtm.pbavu.domain.Client;
+import fr.gtm.pbavu.service.ClientService;
+
+@Controller
+@RequestMapping("/client")
+public class ClientController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ClientController.class);
+
+	@Autowired
+	private ClientService service;
+	
+	
+
+	@PostMapping("/creation/{id}")
+	@ResponseBody
+	public Client add(@RequestBody final Client client) {
+		ClientController.LOGGER.debug("Client ajouter !!", client.getName(), client.getPrenom(), client.getMail(),
+				client.getNumero());
+
+		return this.service.create(client);
+	}
+	
+
+	
+	@PostMapping("/get/{id}")
+	@ResponseBody
+	public Client get(@PathVariable final Integer id) {
+		
+			return this.service.read(id);
+	}
+
+	
+	
+	@RequestMapping({ "/list", "/" })
+	@ResponseBody
+	public List<Client> list() {
+		return this.service.getList();
+	}
+
+}
--- src/main/java/fr/gtm/pbavu/service/ClientService.java
+++ src/main/java/fr/gtm/pbavu/service/ClientService.java
@@ -1,7 +1,20 @@
 package fr.gtm.pbavu.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fr.gtm.pbavu.dao.ClientRepository;
 import fr.gtm.pbavu.domain.Client;
 
+@Service
 public class ClientService extends CRUDService<Client> {
+	
+	
+	
+	
+	
+	
+	
+
 
 }
