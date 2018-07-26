8bb79426da12a0eb41c2578a92ba71079881a8bc
ArnaudVallette <vallette.arnaud@gmail.com>
2018-07-26 19:40:24 +0200

    mise à jour de la vue

--- src/main/java/fr/gtm/pbavu/service/ReponseService.java
+++ src/main/java/fr/gtm/pbavu/service/ReponseService.java
@@ -1,7 +1,10 @@
 package fr.gtm.pbavu.service;
 
+import org.springframework.stereotype.Service;
+
 import fr.gtm.pbavu.domain.Reponse;
 
+@Service
 public class ReponseService extends CRUDService<Reponse> {
 
 }
--- src/main/java/fr/gtm/pbavu/service/SondageService.java
+++ src/main/java/fr/gtm/pbavu/service/SondageService.java
@@ -3,16 +3,21 @@ package fr.gtm.pbavu.service;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import fr.gtm.pbavu.dao.SondageRepository;
 import fr.gtm.pbavu.domain.Sondage;
 
 @Service
 public class SondageService extends CRUDService<Sondage> {
 
+	@Autowired
+	private SondageRepository repo;
+
 	public Sondage getActualSondage(final LocalDate actualDate) {
 		Sondage result = null;
-		final List<Sondage> sondages = this.getList();
+		final List<Sondage> sondages = this.repo.findAll();
 		for (final Sondage sondage : sondages) {
 			if (actualDate.isAfter(sondage.getDateDebut()) && actualDate.isBefore(sondage.getDateFin())) {
 				result = sondage;
