4a31c167411fd7318515b9777b76a2836b8238f4
POEI2018 <nadir.poei2018@gmail.com>
2018-07-26 18:00:16 +0200

    ajout SondageController avec checkSondage et ajout méthode getActualSondage dans sondageService

--- src/main/java/fr/gtm/pbavu/presentation/SondageController.java
+++ src/main/java/fr/gtm/pbavu/presentation/SondageController.java
@@ -1,14 +1,34 @@
 package fr.gtm.pbavu.presentation;
 
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import fr.gtm.pbavu.domain.Sondage;
 import fr.gtm.pbavu.service.SondageService;
 
 @Controller
 @RequestMapping("/sondage")
 public class SondageController {
 
+	@Autowired
 	private SondageService sondageService;
+
+	@GetMapping("")
+	public String checkSondage() {
+		String result = null;
+		final LocalDate actualDate = LocalDate.now();
+		final Sondage actualSondage = this.sondageService.getActualSondage(actualDate);
+		if (actualSondage != null) {
+			result = "Succès sondage : " + actualSondage.getId() + " a commencé le " + actualSondage.getDateDebut()
+					+ " et se termine le " + actualSondage.getDateFermeture();
+		} else {
+			result = "Pas de sondage en cours";
+		}
+		return result;
+	}
 
 }
--- src/main/java/fr/gtm/pbavu/service/SondageService.java
+++ src/main/java/fr/gtm/pbavu/service/SondageService.java
@@ -1,7 +1,23 @@
 package fr.gtm.pbavu.service;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import fr.gtm.pbavu.domain.Sondage;
 
 public class SondageService extends CRUDService<Sondage> {
+
+	public Sondage getActualSondage(final LocalDate actualDate) {
+		Sondage result = null;
+		final List<Sondage> sondages = this.getList();
+		for (final Sondage sondage : sondages) {
+			if (actualDate.isAfter(sondage.getDateDebut()) && actualDate.isBefore(sondage.getDateFin())) {
+				result = sondage;
+			} else {
+				result = null;
+			}
+		}
+		return result;
+	}
 
 }
